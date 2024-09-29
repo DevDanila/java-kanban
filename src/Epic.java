fb0b76ce74263d47719eb3ff759fd2905c52d3c2
User Danila <Gameking99@yandex.ru>
2024-09-29 22:46:31 +0400

    Спринт 4. Финальное задание. 1 итерация.

---
+++ src/Epic.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Epic extends Task {
+	private final ArrayList<Integer> subtaskIds = new ArrayList<>();
+
+	public ArrayList<Integer> getSubtaskIds() {
+		return subtaskIds;
+	}
+
+	public Epic(String title, String description) {
+		super(title, description, Status.NEW);
+	}
+}
+
+
