81fb128258061d2c2c6246f2c7fa816d73ba0b60
User Danila <Gameking99@yandex.ru>
2025-02-02 18:05:05 +0400

    8 спринт, 1 итерация. Выполнено ТЗ 8 спринта. Исправлены тесты и ошибки 2.

--- src/com/practicum/java_kanban/manager/CSVFormat.java
+++ src/com/practicum/java_kanban/manager/CSVFormat.java
@@ -1,6 +1,5 @@
 package com.practicum.java_kanban.manager;
 
-import com.practicum.java_kanban.exceptions.ManagerSaveException;
 import com.practicum.java_kanban.model.*;
 
 import java.time.Duration;
@@ -34,39 +33,39 @@ public class CSVFormat {
 	}
 
 	public static Task fromString(String value) {
-        String[] values = value.split(",");
-        int id = Integer.parseInt(values[0]);
-        TaskType taskType = TaskType.valueOf(values[1]);
-        Status status = Status.valueOf(values[3]);
-        LocalDateTime startTime = values[5].equals("null") ? null : LocalDateTime.parse(values[5]);
-        Duration duration = Duration.ofMinutes(Long.parseLong(values[6]));
-        switch (taskType) {
-            case TaskType.TASK -> {
-                Task task = new Task(values[2], values[4]);
-                task.setId(id);
-                task.setStatus(status);
-                task.setStartTime(startTime);
-                task.setDuration(duration);
-                return task;
-            }
-            case TaskType.EPIC -> {
-                Epic epic = new Epic(values[2], values[4]);
-                epic.setId(id);
-                epic.setStatus(status);
-                epic.setStartTime(startTime);
-                epic.setDuration(duration);
-                return epic;
-            }
-            case TaskType.SUBTASK -> {
-                Subtask subtask = new Subtask(values[2], values[4], Integer.parseInt(values[7]));
-                subtask.setId(id);
-                subtask.setStatus(status);
-                subtask.setStartTime(startTime);
-                subtask.setDuration(duration);
-                return subtask;
-            }
-        }
-        System.err.println("Ошибка в: " + value);
-        return null;
-    }
+		String[] values = value.split(",");
+		int id = Integer.parseInt(values[0]);
+		TaskType taskType = TaskType.valueOf(values[1]);
+		Status status = Status.valueOf(values[3]);
+		LocalDateTime startTime = values[5].equals("null") ? null : LocalDateTime.parse(values[5]);
+		Duration duration = Duration.ofMinutes(Long.parseLong(values[6]));
+		switch (taskType) {
+			case TaskType.TASK -> {
+				Task task = new Task(values[2], values[4]);
+				task.setId(id);
+				task.setStatus(status);
+				task.setStartTime(startTime);
+				task.setDuration(duration);
+				return task;
+			}
+			case TaskType.EPIC -> {
+				Epic epic = new Epic(values[2], values[4]);
+				epic.setId(id);
+				epic.setStatus(status);
+				epic.setStartTime(startTime);
+				epic.setDuration(duration);
+				return epic;
+			}
+			case TaskType.SUBTASK -> {
+				Subtask subtask = new Subtask(values[2], values[4], Integer.parseInt(values[7]));
+				subtask.setId(id);
+				subtask.setStatus(status);
+				subtask.setStartTime(startTime);
+				subtask.setDuration(duration);
+				return subtask;
+			}
+		}
+		System.err.println("Ошибка в: " + value);
+		return null;
+	}
 }
